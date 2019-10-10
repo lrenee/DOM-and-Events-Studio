045eb9f11bb8433ed12f130e430944b682c1a34a
lrenee <lrenee.mo@sbcglobal.net>
2019-10-10 08:08:47 -0500

    still stuck on getting image to move

--- scripts.js
+++ scripts.js
@@ -6,8 +6,26 @@ window.addEventListener("load", function() {
     let background = document.getElementById("shuttleBackground");
     let shuttleHeight = document.getElementById("spaceShuttleHeight");
     let height = Number(shuttleHeight.innerHTML);
+    let image = document.getElementById("rocket");
+    console.log(image.style.top);
+    console.log(image.style.bottom);
+    console.log(image.style.left);
+    console.log(image.style.right);
+    console.log(image.style.current);
+    
+    image.style.bottom = "0px";
+    image.style.left = "0px";
+    image.style.top = "200px";
+    image.style.right = "100px";
+    
+    console.log(image.style.bottom);
+    console.log(image.style.left);
+    
+    // image.style.top = "200px";
     let move = 10;
     let x = 0;
+    let rocketXposition = 0;
+    let rocketYposition = 0;
     let takeoffButton = document.getElementById("takeoff");
     takeoffButton.addEventListener("click", function (event) {
         let response = window.confirm("Please confirm that the shuttle is ready for takeoff.");
@@ -39,17 +57,22 @@ window.addEventListener("load", function() {
     })
     let upButton = document.getElementById("moveUp");
     upButton.addEventListener("click", function (event) {
-        let position = document.getElementById("rocket").offsetTop;
-        if (position <= 0) {
-            document.getElementById("rocket").style.top = "325px";
-            x = 325;
-        } else if (postion <=10) {
-            document.getElementById("rocket").style.top = "0px";
-            x = 0;
-        } else {
-            x = x - move;
-            document.getElementById("rocket").style.top = x + "px";
-        }
+        rocket.style.bottom = document.getElementById("rocket").style.bottom;
+        console.log(rocket.style.bottom);
+
+        rocketYposition = (10;
+        rocketYposition.style.top = rocketYposition + "px"; 
+        // let position = document.getElementById("rocket").offsetTop;
+        // if (position <= 0) {
+        //     document.getElementById("rocket").style.top = "325px";
+        //     x = 325;
+        // } else if (postion <=10) {
+        //     document.getElementById("rocket").style.top = "0px";
+        //     x = 0;
+        // } else {
+        //     x = x - move;
+        //     document.getElementById("rocket").style.top = x + "px";
+        // }
     });
 
 });
